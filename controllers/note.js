4966bbedfe0c50470bbf891e0d1f9e2fb3001e1b
Zwe Thuta <hermez27zz@gmail.com>
2024-09-22 22:29:29 +0630

    upload fixed

--- controllers/note.js
+++ controllers/note.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const Note = require("../models/node");
+const {unlink} = require("../utils/unlink");
 
 exports.getNotes = (req, res, next) => {
   Note.find()
@@ -61,12 +62,15 @@ exports.getNote = (req, res, next) => {
 
 exports.deleteNote = (req, res, next) => {
   const { id } = req.params;
-  Note.findByIdAndDelete(id)
+  Note.findById(id).then(note =>{
+    unlink(note.cover_image)
+    return Note.findByIdAndDelete(id)
     .then((_) => {
       return res.status(204).json({
         message: "Note deleted",
       });
     })
+  })
     .catch((err) => {
       console.log(err);
       res.status(404).json({
@@ -91,10 +95,15 @@ exports.getOldNote = (req, res, next) => {
 
 exports.updateNote = (req, res, next) => {
   const { noteId, title, content } = req.body;
+  const cover_image = req.file;
   Note.findById(noteId)
     .then((note) => {
       note.title = title;
       note.content = content;
+      if(cover_image){
+        unlink(note.cover_image)
+        note.cover_image = cover_image.path;
+      }
       return note.save();
     })
     .then((_) => {
